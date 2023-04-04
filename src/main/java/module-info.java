d9ec8f0d206f6b6eb4da1933b386090f7d04f270
LingASDJ <2735951230@qq.com>
2023-04-04 22:50:25 +0800

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.javafx.card.car {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens com.javafx.card.car to javafx.fxml;
     exports com.javafx.card.car;
